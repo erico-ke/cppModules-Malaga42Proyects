b32d45b13d31ef6b7ce633f60b4f7acc29cd3caa
erico-ke <eliasricokergaravat@gmail.com>
2026-01-18 18:31:25 +0100

    cpp02 ex01. Done

---
+++ cpp02/ex01/includes/Fixed.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/18 17:45:35 by erico-ke          #+#    #+#             */
+/*   Updated: 2026/01/18 18:27:21 by erico-ke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <fstream>
+# include <cmath>
+
+class Fixed
+{
+private:
+	int					_fixedPointNumber;
+	static const int	_fractionalBits = 8;
+public:
+	Fixed(void);
+	Fixed(const int num);
+	Fixed(const float num);
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	~Fixed(void);
+
+	float	toFloat(void) const;
+	int		toInt(void) const;
+};
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
---
+++ cpp02/ex01/src/Fixed.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/18 17:45:40 by erico-ke          #+#    #+#             */
+/*   Updated: 2026/01/18 18:28:09 by erico-ke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+Fixed::Fixed(void) : _fixedPointNumber(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int num)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fixedPointNumber = num <<  _fractionalBits;
+}
+
+Fixed::Fixed(const float num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixedPointNumber = roundf(num * (1 << _fractionalBits));
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed	&Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		_fixedPointNumber = other._fixedPointNumber;
+	return *this;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return  static_cast<float>(_fixedPointNumber) / (1 << _fractionalBits);
+}
+
+int		Fixed::toInt(void) const
+{
+	return _fixedPointNumber / (1 << _fractionalBits);
+}
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return out;
+}
