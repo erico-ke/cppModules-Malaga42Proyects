592199e7eafb1053c32614100f0e3050fb69cd1e
erico-ke <eliasricokergaravat@gmail.com>
2026-02-26 17:12:08 +0100

    cpp04 ex03. Character.cpp needs to be finished

--- cpp04/ex03/src/Character.cpp
+++ cpp04/ex03/src/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:47:42 by erico-ke          #+#    #+#             */
-/*   Updated: 2026/02/26 15:51:13 by erico-ke         ###   ########.fr       */
+/*   Updated: 2026/02/26 17:11:27 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,22 @@
 Character::Character(void) : _name("Default")
 {
 	std::cout << CONSTRUCTOR_COLOR << "Default Character constructor called" << RESET << std::endl;
-	
+	this->_name = "Default";
+	for (int i = 0; i < 4; i++)
+		this->_slot[i] = NULL;
+	for (int i = 0; i < 500; i++)
+		this->_rest[i] = NULL;
 }
+
+Character::Character(std::string name)
+{
+	this->_name = name;
+	for (int i = 0; i < 4; i++)
+		this->_slot[i] = NULL;
+	for (int i = 0; i < 500; i++)
+		this->_rest[i] = NULL;
+}
+
 
 Character::Character(const Character &other)
 {
@@ -29,7 +43,25 @@ Character &Character::operator=(const Character &other)
 	std::cout << CONSTRUCTOR_COLOR << "Character copy assignment operator called" << RESET << std::endl;
 	if (this != &other)
 	{
-		
+		this->_name = getName();
+		for (int i = 0; i < 4; i++)
+		{
+			if (this->_slot[i] != NULL)
+				delete this->_slot[i];
+			if (other._slot[i] != NULL)
+				this->_slot[i] = other._slot[i]->clone();
+			else
+				this->_slot[i] = NULL;
+		}
+		for (int i = 0; i < 500; i++)
+		{
+			if (this->_rest[i] != NULL)
+				delete this->_rest[i];
+			if (other._rest[i] != NULL)
+				this->_rest[i] = other._rest[i]->clone();
+			else
+				this->_rest[i] = NULL;	
+		}
 	}
 	return *this;
 }
@@ -37,4 +69,16 @@ Character &Character::operator=(const Character &other)
 Character::~Character(void)
 {
 	std::cout << DESTRUCTOR_COLOR << "Character destructor called" << RESET << std::endl;
+	for (int i = 0; i < 4; i++)
+		if (this->_slot[i] != NULL)
+			delete this->_slot[i];
+	for (int i = 0; i < 500; i++)
+		if (this->_rest[i] != NULL)
+			delete this->_rest[i];
 }
+
+std::string const&	Character::getName() const
+{
+	return (this->_name);
+}
+
