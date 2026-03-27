e7762eb0fcb82230d26f852b67af02148ef46ac1
erico-ke <eliasricokergaravat@gmail.com>
2026-03-27 13:26:10 +0100

    cpp05 ex00. savepoint

--- cpp05/ex00/src/Bureucrat.cpp
+++ cpp05/ex00/src/Bureucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 12:48:15 by erico-ke          #+#    #+#             */
-/*   Updated: 2026/03/27 12:50:39 by erico-ke         ###   ########.fr       */
+/*   Updated: 2026/03/27 13:23:49 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 Bureucrat::Bureucrat(void)
 {
 	DEBUG_LOG(CONSTRUCTOR_COLOR << "Default Bureucrat constructor called" << RESET);
+}
+
+Bureucrat::Bureucrat(std::string name, int grade)
+{
+	DEBUG_LOG(CONSTRUCTOR_COLOR << "Bureucrat constructor called" << RESET);
 }
 
 Bureucrat::Bureucrat(const Bureucrat &other)
@@ -28,7 +33,7 @@ Bureucrat &Bureucrat::operator=(const Bureucrat &other)
 	DEBUG_LOG(CONSTRUCTOR_COLOR << "Bureucrat copy assignment operator called" << RESET);
 	if (this != &other)
 	{
-		
+		_grade = other._grade;
 	}
 	return *this;
 }
